575d07c0cf9dbd7d47b1b32627420377148b87f0
Виктория Каледа <kaleda99@yandex.ru>
2018-07-22 23:49:08 +0300

    Fix untracked files

---
+++ AMan/wwwroot/js/ImagePreview.js
@@ -0,0 +1,38 @@
+﻿$(document).ready(function () {
+
+    $("#imageBrowse").change(function () {
+
+        var File = this.files;
+
+        if (File && File[0]) {
+            ReadImage(File[0]);
+        }
+        else {
+            $("#targetImg").attr('src', "@Url.Content(Model.AvatarPath ?? "~/Avatars/default.png")");
+
+        }
+    })
+})
+
+
+var ReadImage = function (file) {
+
+    var reader = new FileReader;
+    var image = new Image;
+
+    reader.readAsDataURL(file);
+    reader.onload = function (_file) {
+
+        image.src = _file.target.result;
+        image.onload = function () {
+
+            var height = this.height;
+            var width = this.width;
+            var type = file.type;
+            var size = ~~(file.size / 1024) + "KB";
+
+            $("#targetImg").attr('src', _file.target.result);
+            $("#imagePreview").show();
+        }
+    }
+}
