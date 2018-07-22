ba3164470ae3a22271ba85253e390b62d7e041c8
Виктория Каледа <kaleda99@yandex.ru>
2018-07-23 00:05:25 +0300

    Small fixes

--- AMan/Models/Android.cs
+++ AMan/Models/Android.cs
@@ -1,14 +1,9 @@
-﻿using Microsoft.AspNetCore.Http;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
+﻿using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
 
 namespace AMan.Models
 {
-    public class Android
+	public class Android
     {
 		public int Id { get; set; }
 
--- AMan/Models/Job.cs
+++ AMan/Models/Job.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System.ComponentModel.DataAnnotations;
 
 namespace AMan.Models
 {
-    public class Job
+	public class Job
     {
 		public int Id { get; set; }
 
